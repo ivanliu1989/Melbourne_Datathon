7f03e377142550611a0c0e4f95f09a3a5935f8ae
noahhhhhh <noah.xm.shaw@gmail.com>
2015-10-16 23:09:48 +1100

    understand & data dict

--- README.md
+++ README.md
@@ -6,8 +6,34 @@ Teams have one week to analyse a dataset and come up with exciting new insights.
 
 ![alt tag](http://m.c.lnkd.licdn.com/mpr/mpr/shrink_200_200/p/8/000/1f2/006/28181a6.jpg)
 
+### Self Defined Dictionary
+BET_ID: ID of a Bet
+BET_TRANS_ID: ID of a Transaction in a Bet
+MATCH_BET_ID: ID of a Match
+ACCOUNT_ID: ID of a User
+COUNTRY_OF_RESIDENCE_NAME: Country of a User
+PARENT_EVENT_ID: ID of a Parent Match Event
+EVENT_ID: ID of a Match Event
+MATCH: Name of a Match
+EVENT_NAME: Name of a Match Event
+EVENT_DT: Date of a Match Event
+OFF_DT: ? (What is this date about? from the sample it is the same as EVENT DT)
+BID_TYP: ? (What do they stand for?, e.g. "B ", "L ")
+STATUS_ID: ? (What do they stand for?, e.g. "S " "C " "L ")
+PLACED_DATE: Date when the Bet was placed
+TAKEN_DATE: Date when the Bet was taken/accepted
+SETTLED_DATE: Date when the Bet was settled/officially kept
+CANCELLED_DATE: Date when the Bet was cancelled
+SELECTION_NAME: Name of the team was bidded
+PERSISTENCE_TYPE: ? (What do they stand for?, e.g. NA   "IP")
+BET_PRICE: Price of the Bet
+PRICE_TAKEN: ? (actual Price of the Bet when taken/accepted?)
+INPLAY_BET: Whether the Bet is placed during the match or prior to the match
+BET_SIZE: ? (PRICE_TAKE * Number of Buys?)
+PROFIT_LOSS: Loss of the Bet from BetFair (always a postive number?)
+
 ### Questions
-1. What are BID_TYP, STATUS_ID, SELECTION_NAME, PERSISTENCE_TYPE and INPLAY_BET.
+1. What are BID_TYP, STATUS_ID, SELECTION_NAME, PERSISTENCE_TYPE and INPLAY_BET
 
 ### Thoughts
 1. If time & dates (holiday, weekday) have impacts on people's behaviour (e.g. Bet_size)
--- noah is a soy sause/1_explore.R
+++ noah is a soy sause/1_explore.R
@@ -1,1 +1,53 @@
-print("hello world!")
+##########################################################################
+## 1. setup ##############################################################
+##########################################################################
+setwd("/Volumes/Data Science/Google Drive/data_science_competition/melbourne_datathon/Melbourne_Datathon/")
+require(data.table)
+require(bit64)
+
+##########################################################################
+## 2. read ###############################################################
+##########################################################################
+dt <- fread("data/DSMDatathon_samplefile.txt"
+            , na.strings = c("NA", "")
+            , colClasses = c("interger","interger","interger","interger","character","interger","interger",
+                              "character","character","character","character","character","character","character",
+                              "character","character","character","character","character","numeric","numeric",
+                              "character","numeric","numeric"))
+
+##########################################################################
+## 3. inspect ############################################################
+##########################################################################
+View(dt)
+## number of NAs
+apply(dt
+      , 2
+      , function(x){sum(is.na(x))})
+# MATCH_BET_ID 5755
+# TAKEN_DATE 5755
+# CANCELLED_DATE 18190
+# PERSISTENCE_TYPE 22702
+# PRICE_TAKEN 5755
+# PROFIT_LOSS 5755
+
+## number of unique values (a snap of the relationship among cols)
+apply(dt
+      , 2
+      , function(x){length(unique(x))})
+# BET_ID              BET_TRANS_ID              MATCH_BET_ID 
+# 14479                      23691                     12359 
+# ACCOUNT_ID COUNTRY_OF_RESIDENCE_NAME           PARENT_EVENT_ID 
+# 2523                              19                         1 
+# EVENT_ID                     MATCH                EVENT_NAME 
+# 1                                1                         1 
+# EVENT_DT                    OFF_DT                   BID_TYP 
+# 1                                1                         2 
+# STATUS_ID               PLACED_DATE                TAKEN_DATE 
+# 3                              1419                      1133 
+# SETTLED_DATE            CANCELLED_DATE            SELECTION_NAME 
+# 1                                  913                         2 
+# PERSISTENCE_TYPE                 BET_PRICE               PRICE_TAKEN 
+# 2                                      274                       205 
+# INPLAY_BET                  BET_SIZE               PROFIT_LOSS 
+# 2                               5973                      7681
+dim(dt)
