8079d227662a58e877104f88a576cafb3de96a0b
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2015-10-16 20:18:40 +1100

    read data

---
+++ 1_Data_exploration.R
@@ -0,0 +1,11 @@
+setwd('/Users/ivanliu/Google Drive/Melbourne Datathon/Melbourne_Datathon')
+rm(list=ls()); gc()
+
+require(data.table); require(bit64)
+
+trans <- fread('data/DSMDatathon_samplefile.txt', data.table = F, stringsAsFactors = F, 
+               colClasses = c('interger','interger','interger','interger','character','interger','interger',
+                              'character','character','character','character','character','character','character',
+                              'character','character','character','character','character','numeric','numeric',
+                              'character','numeric','numeric'))
+head(trans); dim(trans)
