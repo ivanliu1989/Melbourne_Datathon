485d1477a6b3ca6cfcf9387b3a41602b6e1d55fc
noahhhhhh <noah.xm.shaw@gmail.com>
2015-10-19 12:49:16 +1100

    a little more

--- noah is a soy sause/1_explore.R
+++ noah is a soy sause/1_explore.R
@@ -1,55 +1,69 @@
 ##########################################################################
 ## 1. setup ##############################################################
 ##########################################################################
-setwd("/Volumes/Data Science/Google Drive/data_science_competition/melbourne_datathon/Melbourne_Datathon/")
+## thanks to Ivan!
+rm(list = ls()); gc()
 require(data.table)
-require(bit64)
+setwd("/Volumes/Data Science/Google Drive/data_science_competition/melbourne_datathon/Melbourne_Datathon/")
 
 ##########################################################################
-## 2. read ###############################################################
+## 2. prepare, ref:ivan ##################################################
 ##########################################################################
-dt <- fread("data/DSMDatathon_samplefile.txt"
-            , na.strings = c("NA", "")
-            , colClasses = c("interger","interger","interger","interger","character","interger","interger",
-                              "character","character","character","character","character","character","character",
-                              "character","character","character","character","character","numeric","numeric",
-                              "character","numeric","numeric"))
+## ivan is the data science god! #######
+system("ls ../Datathon_Full_Dataset/")
+system("head ../Datathon_Full_Dataset/*.csv")
 
+colClasses <- c(rep('interger',4),'character',rep('interger',2),rep('character',12),rep('numeric',2),'character',rep('numeric',2))
+dt_1 <- fread('../Datathon_Full_Dataset/Datathon WC Data Games 1-10.csv', colClasses=colClasses, na.strings = c("NA","N/A",""))
+dt_2 <- fread('../Datathon_Full_Dataset/Datathon WC Data Games 11-20.csv', colClasses=colClasses, na.strings = c("NA","N/A",""))
+dt_3 <- fread('../Datathon_Full_Dataset/Datathon WC Data Games 21-30.csv', colClasses=colClasses, na.strings = c("NA","N/A",""))
+dt_4 <- fread('../Datathon_Full_Dataset/Datathon WC Data Games 31-40.csv', colClasses=colClasses, na.strings = c("NA","N/A",""))
+dt_5 <- fread('../Datathon_Full_Dataset/Datathon WC Data Games QTR Finals.csv',colClasses=colClasses, na.strings = c("NA","N/A",""))
+
+########################
+### 2. Data Cleasing ###
+########################
+# 2.1 Clean up column names
+identical(names(dt_1), names(dt_3))
+setnames(dt_1, names(dt_1), c('BET_ID', names(dt_1)[-1]))
+setnames(dt_2, names(dt_2), c('BET_ID', names(dt_2)[-1]))
+setnames(dt_3, names(dt_3), c('BET_ID', names(dt_3)[-1]))
+setnames(dt_4, names(dt_4), c('BET_ID', names(dt_4)[-1]))
+setnames(dt_5, names(dt_5), c('BET_ID', names(dt_5)[-1]))
+str(dt_1)
+
+# 2.2 Fix PROFIT_LOSS Calc
+dt_1[,c('PROFIT_LOSS','table_num') := list(ifelse(as.numeric(PROFIT_LOSS) > 0, ifelse(gsub(" *$", "", BID_TYP) == "B", (as.numeric(PRICE_TAKEN)-1)*as.numeric(BET_SIZE), as.numeric(BET_SIZE)),0) + 
+                                               ifelse(as.numeric(PROFIT_LOSS) < 0, ifelse(gsub(" *$", "", BID_TYP) == "L", (as.numeric(PRICE_TAKEN)-1)*-as.numeric(BET_SIZE), -as.numeric(BET_SIZE)),0), 1)]
+dt_2[,c('PROFIT_LOSS','table_num') := list(ifelse(as.numeric(PROFIT_LOSS) > 0, ifelse(gsub(" *$", "", BID_TYP) == "B", (as.numeric(PRICE_TAKEN)-1)*as.numeric(BET_SIZE), as.numeric(BET_SIZE)),0) + 
+                                               ifelse(as.numeric(PROFIT_LOSS) < 0, ifelse(gsub(" *$", "", BID_TYP) == "L", (as.numeric(PRICE_TAKEN)-1)*-as.numeric(BET_SIZE), -as.numeric(BET_SIZE)),0), 2)]
+dt_3[,c('PROFIT_LOSS','table_num') := list(ifelse(as.numeric(PROFIT_LOSS) > 0, ifelse(gsub(" *$", "", BID_TYP) == "B", (as.numeric(PRICE_TAKEN)-1)*as.numeric(BET_SIZE), as.numeric(BET_SIZE)),0) + 
+                                               ifelse(as.numeric(PROFIT_LOSS) < 0, ifelse(gsub(" *$", "", BID_TYP) == "L", (as.numeric(PRICE_TAKEN)-1)*-as.numeric(BET_SIZE), -as.numeric(BET_SIZE)),0), 3)]
+dt_4[,c('PROFIT_LOSS','table_num') := list(ifelse(as.numeric(PROFIT_LOSS) > 0, ifelse(gsub(" *$", "", BID_TYP) == "B", (as.numeric(PRICE_TAKEN)-1)*as.numeric(BET_SIZE), as.numeric(BET_SIZE)),0) + 
+                                               ifelse(as.numeric(PROFIT_LOSS) < 0, ifelse(gsub(" *$", "", BID_TYP) == "L", (as.numeric(PRICE_TAKEN)-1)*-as.numeric(BET_SIZE), -as.numeric(BET_SIZE)),0), 4)]
+dt_5[,c('PROFIT_LOSS','table_num') := list(ifelse(as.numeric(PROFIT_LOSS) > 0, ifelse(gsub(" *$", "", BID_TYP) == "B", (as.numeric(PRICE_TAKEN)-1)*as.numeric(BET_SIZE), as.numeric(BET_SIZE)),0) + 
+                                               ifelse(as.numeric(PROFIT_LOSS) < 0, ifelse(gsub(" *$", "", BID_TYP) == "L", (as.numeric(PRICE_TAKEN)-1)*-as.numeric(BET_SIZE), -as.numeric(BET_SIZE)),0), 5)]
+
+# 2.3 Merge Datasets
+dt <- rbind(dt_1, dt_2, dt_3, dt_4, dt_5)
+dim(dt_1); dim(dt_2); dim(dt_3); dim(dt_4); dim(dt_5); dim(dt)
+
+# 2.4 Regular Expression
+dt[, c('BID_TYP', 'STATUS_ID') := 
+       list(gsub(" *$", "", BID_TYP), gsub(" *$", "", STATUS_ID))]
+str(dt)
+## ivan is the data science god! #######
+
+
+dt <- data.table(dt)
 ##########################################################################
 ## 3. inspect ############################################################
 ##########################################################################
-View(dt)
-## number of NAs
-apply(dt
-      , 2
-      , function(x){sum(is.na(x))})
-# MATCH_BET_ID 5755
-# TAKEN_DATE 5755
-# CANCELLED_DATE 18190
-# PERSISTENCE_TYPE 22702
-# PRICE_TAKEN 5755
-# PROFIT_LOSS 5755
-
-## number of unique values (a snap of the relationship among cols)
-apply(dt
-      , 2
-      , function(x){length(unique(x))})
-# BET_ID              BET_TRANS_ID              MATCH_BET_ID 
-# 14479                      23691                     12359 
-# ACCOUNT_ID COUNTRY_OF_RESIDENCE_NAME           PARENT_EVENT_ID 
-# 2523                              19                         1 
-# EVENT_ID                     MATCH                EVENT_NAME 
-# 1                                1                         1 
-# EVENT_DT                    OFF_DT                   BID_TYP 
-# 1                                1                         2 
-# STATUS_ID               PLACED_DATE                TAKEN_DATE 
-# 3                              1419                      1133 
-# SETTLED_DATE            CANCELLED_DATE            SELECTION_NAME 
-# 1                                  913                         2 
-# PERSISTENCE_TYPE                 BET_PRICE               PRICE_TAKEN 
-# 2                                      274                       205 
-# INPLAY_BET                  BET_SIZE               PROFIT_LOSS 
-# 2                               5973                      7681
+head(dt)
+tail(dt)
+str(dt)
 dim(dt)
+# [1] 3461173      26
 
-## 
+require(sqldf)
+dt_successTrans <- sqldf("SELECT * FROM dt A JOIN dt B ON A.BET_ID = B.MATCH_BET_ID AND A.MATCH_BET_ID = B.BET_ID")
