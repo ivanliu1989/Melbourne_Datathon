ca4c79634b58cc7ff88c4146cf277583376b99b1
noahhhhhh <noah.xm.shaw@gmail.com>
2015-10-19 21:20:44 +1100

    cluster with simple kmeans

--- noah is a soy sause/3_visualisation.R
+++ noah is a soy sause/3_visualisation.R
@@ -1,3 +1,4 @@
+## WILL BE BACK AFTER SHOWER
 rm(list = ls()); gc()
 load("../Datathon_Full_Dataset/subsetRateAccount_noTail.RData")
 load("../Datathon_Full_Dataset/subsetRateLoc.RData")
@@ -19,6 +20,20 @@ hist(subsetRateAccount_noTail$WINRATE) # a very balanced histgram
 hist(subsetRateAccount_noTail$PROFIT_LOSS_L + subsetRateAccount_noTail$PROFIT_LOSS_W) # a long head of loss
 hist(subsetRateAccount_noTail$CNT_L + subsetRateAccount_noTail$CNT_W) # a long tail of large no. of transactions
 
+# cluster with kmeans
+subsetRateAccount_noTailForKmeans <- subsetRateAccount_noTail
+subsetRateAccount_noTailForKmeans$CNT_TRANS <- subsetRateAccount_noTailForKmeans$CNT_L + subsetRateAccount_noTailForKmeans$CNT_W
+subsetRateAccount_noTailForKmeans$PROFIT_LOSS <- subsetRateAccount_noTailForKmeans$PROFIT_LOSS_L + subsetRateAccount_noTailForKmeans$PROFIT_LOSS_W
+subsetRateAccount_noTailForKmeans <- as.data.table(cbind(ACCOUNT_ID = subsetRateAccount_noTailForKmeans$ACCOUNT_ID, scale(subsetRateAccount_noTailForKmeans[, -c(1, 2), with = F])))
+
+kmAcct.out <- kmeans(subsetRateAccount_noTailForKmeans[, -1, with = F]
+                             , 5
+                             , nstart = 50)
+
+plot(subsetRateAccount_noTailForKmeans[, c("PROFIT_LOSS_W", "CNT_W"), with = F]
+     , col=(kmAcct.out$cluster)
+     , main="K-Means Clustering Results with K = 5", xlab="PROFIT_LOSS_W", ylab="CNT_W", pch=20, cex=2)
+
 ## 1.2 LOC oriented graph
 str(subsetRateLoc)
 # $ COUNTRY_OF_RESIDENCE_NAME: chr  "United Kingdom" "Australia" "Malta" "India" ...
@@ -27,11 +42,33 @@ str(subsetRateLoc)
 # $ CNT_TRANS_W              : int  379408 53681 133294 24912 212364 161389 4874 246165 817 1183 ...
 # $ PROFIT_LOSS_L            : num  -530838 -159824 -280696 -30856 -203652 ...
 # $ PROFIT_LOSS_W            : num  569546 176694 285856 41052 188122 ...
+# $ CNT_TRANS_PER_ACCT       : num  208.4 99.5 218.7 124.2 697.4 ...
 # $ PROFIT_LOSS_PER_ACCT     : num  10.5 15.16 4.15 22.76 -26.82 ...
 # $ WINRATE                  : num  0.494 0.485 0.49 0.448 0.526 ...
 
 ## just take a look
 hist(subsetRateLoc$CNT_ACCOUNT) # a long tail of big no. of accounts
+hist(subsetRateLoc$WINRATE)
 
-## keep going after dinner!!!
+## cluster with kmeans
+subsetRateLocForKmeans <- subsetRateLoc
+subsetRateLocForKmeans$CNT_TRANS <- subsetRateLocForKmeans$CNT_TRANS_L + subsetRateLocForKmeans$CNT_TRANS_W
+subsetRateLocForKmeans$PROFIT_LOSS <- subsetRateLocForKmeans$PROFIT_LOSS_L + subsetRateLocForKmeans$PROFIT_LOSS_W
+subsetRateLocForKmeans <- subsetRateLocForKmeans[, !c("CNT_TRANS_L", "CNT_TRANS_W", "PROFIT_LOSS_L", "PROFIT_LOSS_W"), with = F]
+subsetRateLocForKmeans <- as.data.table(cbind(subsetRateLocForKmeans$COUNTRY_OF_RESIDENCE_NAME, scale(subsetRateLocForKmeans[, -1, with = F])))
+
+kmLocAcctTrans.out <- kmeans(subsetRateLocForKmeans[, c("CNT_TRANS_PER_ACCT", "WINRATE"), with = F]
+                             , 5
+                             , nstart = 50)
+
+plot(subsetRateLocForKmeans[, c("WINRATE", "CNT_TRANS_PER_ACCT"), with = F]
+     , col=(kmLocAcctTrans.out$cluster)
+     , main="K-Means Clustering Results with K = 5", xlab="WINRATE", ylab="CNT_TRANS_PER_ACCT", pch=20, cex=2)
+
+##
+
+
+
+
+
 
